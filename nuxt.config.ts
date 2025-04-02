c15c9e197fb8a1358253514bd66f01a9cb36e19d
mirsella <mirsella@protonmail.com>
2025-04-02 15:07:28 +0200

    migrate to tailwind v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
-  ssr: false,
+	compatibilityDate: "2024-04-03",
+	devtools: { enabled: true },
+	css: ["~/assets/css/tailwind.css"],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	ssr: false,
 });
