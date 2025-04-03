b10be99670af1f26390b29f5fbd7368f10ce5650
mirsella <mirsella@protonmail.com>
2025-04-03 11:08:23 +0200

    better header

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+const user = useAuth().user;
+</script>
 <template>
   <div class="bg-base-200 min-h-screen">
-    <div class="w-full p-8">swifty protein</div>
+    <div class="mb-4 bg-base-100 navbar shadow-sm">
+      <div class="flex-1">swifty protein</div>
+      <div class="mx-4 badge" v-if="user">{{ user?.username }}</div>
+      <button class="mx-4 btn btn-error" @click="useAuth().logout()">
+        Logout
+      </button>
+    </div>
     <NuxtPage />
   </div>
 </template>
