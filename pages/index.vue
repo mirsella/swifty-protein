ba39bdf5e803baa2a6a691e8084b9b674b343041
mirsella <mirsella@protonmail.com>
2025-04-04 09:41:13 +0200

    download ligand file from rcsb on click

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,54 @@ const ligands_filtered = computed(() => {
   return ligands.value;
 });
 
+const error = ref("");
+const error_modal = useTemplateRef("error_modal");
+error_modal.value?.addEventListener("close", () => {
+  error.value = "";
+});
+watch(error, () => {
+  if (error.value.length > 0) {
+    error_modal.value?.showModal();
+  }
+});
+
 const search = ref("");
+const loading = ref<null | string>(null);
+
+async function fetchLigand(ligand: string) {
+  if (loading.value === ligand) {
+    return;
+  }
+  loading.value = ligand;
+  try {
+    const response = await fetch(
+      `https://files.rcsb.org/ligands/download/${ligand}.cif`,
+    );
+    // TODO: use it in the protein viewer page
+    console.log(await response.text());
+  } catch (e) {
+    console.error(e);
+    error.value = (e as Error).message;
+  } finally {
+    loading.value = null;
+  }
+}
 </script>
 <template>
   <div class="flex flex-col items-center justify-center mx-4">
-    <div class="input input-secondary mb-4">
+    <dialog ref="error_modal" class="modal">
+      <div class="modal-box space-y-4 !px-20 max-w-lg">
+        <h3 class="text-lg font-bold text-center">
+          Loading ligand {{ loading }} failed
+        </h3>
+        <h1 class="font-semibold text-error text-center">Error: {{ error }}</h1>
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+
+    <div class="input input-secondary my-2">
       <span class="i-carbon-search">Ligands</span>
       <input type="search" v-model="search" class="grow" placeholder="Search" />
       <button
@@ -23,9 +66,17 @@ const search = ref("");
         @click="search = ''"
       ></button>
     </div>
-    <div class="flex gap-2 flex-wrap w-full justify-center items-center">
-      <button v-for="ligand in ligands_filtered" class="btn btn-secondary">
-        {{ ligand }}
+
+    <div class="flex gap-2 flex-wrap w-full justify-center items-center my-2">
+      <button
+        v-for="ligand in ligands_filtered"
+        class="btn btn-secondary"
+        :disabled="loading !== ligand && loading !== null"
+        @click="fetchLigand(ligand)"
+      >
+        <span :class="{ 'loading loading-spinner': loading === ligand }">
+          {{ ligand }}
+        </span>
       </button>
     </div>
   </div>
