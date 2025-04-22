7d48ecd9fde0feb1fa7b1a3cb2b81e1cf8d843cc
kbagot <kevin.bagot@gmail.com>
2025-04-22 11:31:34 +0200

    add return to header

--- app.vue
+++ app.vue
@@ -14,10 +14,10 @@ App.addListener("appStateChange", () => {
 </script>
 <template>
   <div class="flex flex-col min-h-screen">
-    <div
-      class="mb-4 gap-8 bg-base-100 navbar shadow-sm pt-[env(safe-area-inset-top)]"
-    >
-      <div class="flex-1 mx-4 font-semibold text-xl">swifty protein</div>
+    <div class="mb-4 gap-8 bg-base-100 navbar shadow-sm pt-[env(safe-area-inset-top)]">
+      <div class="flex-1 mx-4 font-semibold text-xl cursor-pointer" @click="navigateTo('/')">
+        swifty protein
+      </div>
       <div v-if="user">
         <div class="mx-2 badge">{{ user?.username }}</div>
         <button class="mx-2 btn btn-error" @click="useAuth().logout()">
